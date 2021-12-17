e5c891838e6b200bf73dab08f432d65204c387c7
ottolau <ka_tung_lau@brown.edu>
2021-12-17 06:26:15 +0100

    use likelihood from fit

--- maximum_likelihood_singleChannel.py
+++ maximum_likelihood_singleChannel.py
@@ -6,15 +6,29 @@ rt.gROOT.SetBatch(True);
 rt.gROOT.SetStyle("Plain");
 from plotting import *
 
-def createWorkspace(wsname, wsfilename, N=0.0, n_norm=0.0, dn_norm=0.0, eff_tar=0.0, deff_tar=0.0, eff_norm=0.0, deff_norm=0.0):
+def importWorkspace(wspace, wsfilename_import, rename):
+    wsfile_import = rt.TFile.Open(wsfilename_import)
+    wspace_import = wsfile_import.Get('wspace')
+    data_import = wspace_import.data('data')
+    getattr(wspace, 'import')(data_import, rt.RooCmdArg(RooFit.Rename('data_{}'.format(rename))))
+    model_import = wspace_import.pdf('model')
+    getattr(wspace, 'import')(model_import, RooFit.RenameAllVariablesExcept(rename, 'x'), RooFit.RenameAllNodes(rename))
+    return wspace
+
+def createWorkspace(wsname, wsfilename, wsfilename_tar, wsfilename_norm, n_norm=0.0, dn_norm=0.0, eff_tar=0.0, deff_tar=0.0, eff_norm=0.0, deff_norm=0.0):
     wspace = rt.RooWorkspace(wsname)
+    wspace = importWorkspace(wspace, wsfilename_tar, 'tar')
+    wspace = importWorkspace(wspace, wsfilename_norm, 'norm')
+
+    wspace.factory('cat[tar,norm]')
+    x = wspace.var("x")
+    data = ROOT.RooDataSet('data', 'data', rt.RooArgSet(x), RooFit.Index(wspace.cat('cat')), RooFit.Import('tar', wspace.data('data_tar')), RooFit.Import('norm', wspace.data('data_norm')))
+    getattr(wspace, 'import')(data, rt.RooCmdArg())
+
 
     ### Create parameters
     # observations
-    params = [('N',       N,     0,  20000),
-    # number of signal in normalization channel estimate        
-              ('n_hat_norm',     n_norm,   0,  20000),
-              ('dn_norm',       dn_norm,   0,   5000),
+    params = [
     # efficiency of target channel estimate
               ('eff_hat_tar',   eff_tar,   0,   1),
               ('deff_tar',     deff_tar,   0,   1),
@@ -26,7 +40,7 @@ def createWorkspace(wsname, wsfilename, N=0.0, n_norm=0.0, dn_norm=0.0, eff_tar=
               ('eff_tar',       eff_tar,   0,  1),
               ('eff_norm',     eff_norm,   0,  1),
     # parameter of interest
-              ('R',      0.08,    0,   1)]
+              ('R',      0.09,    0.06,   0.15)]
 
     for t in params:
         cmd = '%s[%f, %f, %f]' % t
@@ -34,26 +48,33 @@ def createWorkspace(wsname, wsfilename, N=0.0, n_norm=0.0, dn_norm=0.0, eff_tar=
     wspace.var('R').SetTitle('R')
 
     # fix all background and signal parameters
-    for t in params[1:-4]:
+    for t in params[0:-4]:
         name = t[0]
         print '=> make %8s = %5.5f constant' % (name,
                                                 wspace.var(name).getVal())
         wspace.var(name).setConstant()
 
     ### Create expressions
-    express = ['M_norm("(n_hat_norm/dn_norm)^2", n_hat_norm, dn_norm)',
-               'tau_norm("n_hat_norm/dn_norm^2", n_hat_norm, dn_norm)',
-               'tau_norm_n("tau_norm*n_norm", tau_norm, n_norm)',
-               'n("R*n_norm*(eff_tar/eff_norm)", R, n_norm, eff_tar, eff_norm)']
+    express = ['n_tar("R*n_norm*(eff_tar/eff_norm)", R, n_norm, eff_tar, eff_norm)',
+               'nKstarPsi2S_tar_modified("frac_partial_tar*n_tar", frac_partial_tar, n_tar)',
+               'nKstarJpsi_norm_modified("frac_partial_norm*n_norm", frac_partial_norm, n_norm)',
+              ]
         
     for t in express:
         cmd = 'expr::%s' % t
         wspace.factory(cmd)
 
+    edit = ['model_tar_modified(model_tar, nsignal_tar=n_tar, nKstarPsi2S_tar=nKstarPsi2S_tar_modified)',
+            'model_norm_modified(model_norm, nsignal_norm=n_norm, nKstarJpsi_norm=nKstarJpsi_norm_modified)',
+           ]
+
+    for t in edit:
+        cmd = 'EDIT::%s' % t
+        wspace.factory(cmd)
+
+    wspace.Print("V")
     ### Create pdfs
-    pdfs = [('Poisson', 'pN',    '(N, n)'),
-            ('Poisson', 'pM_norm', '(M_norm, tau_norm_n, 1)'), 
-            ('Gaussian','peff_tar', '(eff_hat_tar, eff_tar, deff_tar)'),
+    pdfs = [('Gaussian','peff_tar', '(eff_hat_tar, eff_tar, deff_tar)'),
             ('Gaussian','peff_norm', '(eff_hat_norm, eff_norm, deff_norm)'),
            ]
     
@@ -64,28 +85,20 @@ def createWorkspace(wsname, wsfilename, N=0.0, n_norm=0.0, dn_norm=0.0, eff_tar=
         prodpdf += "%s, " % name
     prodpdf = prodpdf[:-2] # remove last ", "
     
+
     # multiply the pdfs together. use upper case PROD to
     # do this
-    wspace.factory('PROD::model(%s)' % prodpdf)
+    wspace.factory("SIMUL:jointModel(cat,tar=model_tar_modified,norm=model_norm_modified)")
+    wspace.factory('PROD::model({},{})'.format(prodpdf, 'jointModel'))
 
-    sets = [('obs',  'N'),           # observations
+    sets = [('obs',  'x'),           # observations
             ('poi',  'R'),          # parameter of interest
             ('nuis', 'n_norm,eff_tar,eff_norm')] # nuisance parameters (leave no spaces)
     for t in sets:
         name, parlist = t
         wspace.defineSet(name, parlist)
     
-    #-----------------------------------------------------        
-    # create a dataset
-    #-----------------------------------------------------    
-    data = rt.RooDataSet('data', 'data', wspace.set('obs'))
-    data.add(wspace.set('obs'))
-    # import dataset into workspace
-    # need last argument to workaround a PyROOT "feature".
-    # the last argument ensures the correct version of
-    # the import method is called.
-    getattr(wspace, 'import')(data, rt.RooCmdArg())
-        
+      
     #-----------------------------------------------------
     # Create model configuration. This is needed for the
     # statistical analyses
@@ -121,17 +134,18 @@ def analyzeWorkspace(wsname, wsfilename, R_SM=None):
     #-----------------------------------------------------    
     # Fit model to data
     #-----------------------------------------------------
-    results = wspace.pdf('model').fitTo(data, rt.RooFit.Save())
+    results = wspace.pdf('model').fitTo(data, RooFit.Extended(True), rt.RooFit.Save())
     results.Print()
     
     #-----------------------------------------------------    
     # Compute interval based on profile likelihood
     #-----------------------------------------------------
     # suppress some (apparently) innocuous warnings
-    msgservice = rt.RooMsgService.instance()
-    msgservice.setGlobalKillBelow(rt.RooFit.FATAL)
+    #msgservice = rt.RooMsgService.instance()
+    #msgservice.setGlobalKillBelow(rt.RooFit.FATAL)
        
     R_min, R_max = 0.07, 0.12
+    NPoints = 20
     xlabel = 'R_{#psi (2S)}^{e}'
 
     print 'compute interval using profile likelihood'
@@ -155,7 +169,8 @@ def analyzeWorkspace(wsname, wsfilename, R_SM=None):
     plcplot.SetRange(R_min, R_max)
     plcplot.SetMaximum(10)
     plcplot.SetLineColor(9)
-    plcplot.Draw()
+    plcplot.SetNPoints(NPoints)
+    plcplot.Draw("tf1")
 
     # compute an 95% limit on mu by
     CL = 0.95
@@ -172,7 +187,8 @@ def analyzeWorkspace(wsname, wsfilename, R_SM=None):
     plcplot2.SetRange(R_min, R_max)
     plcplot2.SetMaximum(10)
     plcplot2.SetLineColor(8)
-    plcplot2.Draw("same")
+    plcplot2.SetNPoints(NPoints)
+    plcplot2.Draw("tf1 same")
     
     frame = plcplot.GetPlottedObject()
     frame.GetYaxis().SetTitle('Profile of -log(L/L_{min})')
@@ -188,9 +204,12 @@ def analyzeWorkspace(wsname, wsfilename, R_SM=None):
     frame.GetXaxis().SetLabelSize(0.04)
     frame.GetXaxis().SetLabelFont(42)
 
+    pad.Update()
+    uymax = rt.gPad.GetUymax()
+    uymin = rt.gPad.GetUymin()
     if R_SM is not None:
       pad.cd()
-      l = rt.TLine(R_SM, 0, R_SM, 10)
+      l = rt.TLine(R_SM, uymin, R_SM, uymax)
       l.SetLineColor(2)
       l.SetLineWidth(2)
       l.Draw("same")
@@ -213,9 +232,11 @@ if __name__ == "__main__":
     eff_norm, deff_norm = 0.03680, 0.00023
     R_SM = 0.0812
     
+    wsfilename_tar = 'wspace_psi2s_fixedPartial_pf_wp5.0.root'
+    wsfilename_norm = 'wspace_jpsi_fixedPartial_pf_wp5.0.root'
     wsfilename = 'wspace_rpsi2s_electron.root'
 
-    createWorkspace('R_psi2s', wsfilename, N=N, n_norm=n_norm, dn_norm=dn_norm, eff_tar=eff_tar, deff_tar=deff_tar, eff_norm=eff_norm, deff_norm=deff_norm)
+    createWorkspace('R_psi2s', wsfilename, wsfilename_tar, wsfilename_norm, n_norm=n_norm, dn_norm=dn_norm, eff_tar=eff_tar, deff_tar=deff_tar, eff_norm=eff_norm, deff_norm=deff_norm)
     plccanvas = analyzeWorkspace('R_psi2s', wsfilename, R_SM=R_SM)
 
 
