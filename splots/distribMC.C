59cac6545868196ed9fe6ba748b1d17763fb5898
crovelli <chiara.rovelli@cern.ch>
2022-02-09 16:40:09 +0100

    first commit

---
+++ splots/distribMC.C
@@ -0,0 +1,129 @@
+#define distribMC_cxx
+#include "distribMC.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TLegend.h>   
+#include <iostream>  
+
+// To be run on MC tnp formatted ntuples to get distributions for 
+// comparison with sPlots 
+// Select signal and background based on match with MC-truth 
+
+void distribMC::Loop(int q2bin, float bdtCut)
+{
+  // Define q^2 ranges
+  float q2inf = 0;
+  float q2sup = 1000;
+  if (q2bin==0) { q2inf=1.05; q2sup=2.45; }
+  if (q2bin==1) { q2inf=2.90; q2sup=3.20; }
+  if (q2bin==2) { q2inf=3.55; q2sup=3.80; }
+  if (q2bin==3) { q2inf=4.00; q2sup=4.80; }
+
+  if (fChain == 0) return;
+
+  float theinf = bdtCut-2;
+  float thedelta = 15.-theinf;
+  int thebin = thedelta/0.5;
+  TH1F *h1mc_mll    = new TH1F("h1mc_mll","h1mc_mll",60,  1., 5.);
+  TH1F *h1mc_bmass  = new TH1F("h1mc_bmass","h1mc_bmass",60,  4.5, 5.9);
+  TH1F *h1mc_xgb    = new TH1F("h1mc_xgb","h1mc_xgb",thebin,theinf,15);
+  TH1F *h1mc_L1id   = new TH1F("h1mc_L1id","h1mc_L1id",33,-4.,7.);
+  TH1F *h1mc_L2id   = new TH1F("h1mc_L2id","h1mc_L2id",33,-4.,7.);
+  TH1F *h1mc_Bprob  = new TH1F("h1mc_Bprob","h1mc_Bprob",10,0.,1.);
+  TH1F *h1mc_BsLxy  = new TH1F("h1mc_BsLxy","h1mc_BsLxy",10,0.,100.);
+  TH1F *h1mc_Bcos   = new TH1F("h1mc_Bcos","h1mc_Bcos",10,0.99,1.);
+  TH1F *h1mc_L1pt   = new TH1F("h1mc_L1pt","h1mc_L1pt",60,0.,30.);
+  TH1F *h1mc_L2pt   = new TH1F("h1mc_L2pt","h1mc_L2pt",40,0.,20.);
+  TH1F *h1mc_Kpt    = new TH1F("h1mc_Kpt","h1mc_Kpt",40,0.,20.);
+  TH1F *h1mc_LKdz   = new TH1F("h1mc_LKdz","h1mc_LKdz",20,0.,1.);
+  TH1F *h1mc_L1L2dr = new TH1F("h1mc_L1L2dr","h1mc_L1L2dr",20,0.,2.);
+  TH1F *h1mc_LKdr   = new TH1F("h1mc_LKdr","h1mc_LKdr",40,0.,1.);
+  TH1F *h1mc_L1iso  = new TH1F("h1mc_L1iso","h1mc_L1iso",30,0.,30.);
+  TH1F *h1mc_Kiso   = new TH1F("h1mc_Kiso","h1mc_Kiso",30,0.,30.);
+  TH1F *h1mc_Kip3d  = new TH1F("h1mc_Kip3d","h1mc_Kip3d",40,-0.2,0.2);
+  TH1F *h1mc_Passymetry = new TH1F("h1mc_Passymetry","h1mc_Passymetry",20,-1.,1.);
+  h1mc_mll    -> Sumw2(); 
+  h1mc_bmass  -> Sumw2(); 
+  h1mc_xgb    -> Sumw2(); 
+  h1mc_L1id   -> Sumw2();
+  h1mc_L2id   -> Sumw2();
+  h1mc_Bprob  -> Sumw2();
+  h1mc_BsLxy  -> Sumw2();
+  h1mc_Bcos   -> Sumw2();
+  h1mc_L1pt   -> Sumw2();
+  h1mc_L2pt   -> Sumw2();
+  h1mc_Kpt    -> Sumw2();
+  h1mc_LKdz   -> Sumw2();
+  h1mc_L1L2dr -> Sumw2();
+  h1mc_LKdr   -> Sumw2();
+  h1mc_L1iso  -> Sumw2();
+  h1mc_Kiso   -> Sumw2();
+  h1mc_Kip3d  -> Sumw2();
+  h1mc_Passymetry -> Sumw2();
+
+  // Loop over entries
+  Long64_t nentries = fChain->GetEntriesFast();
+  Long64_t nbytes = 0, nb = 0;
+  cout << "nentries = " << nentries << endl;
+
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   
+    nbytes += nb;
+
+    // Same selection as applied in data
+    if (Bmass>5.7 || Bmass<4.7) continue;
+    if (Mll>q2sup || Mll<q2inf) continue;
+    if (xgb<bdtCut)   continue;
+    if (KLmassD0<2.0) continue;
+
+    // HLT and MC-match should be applied already at ntuple level
+
+    // Fill all histos
+    h1mc_mll         -> Fill(Mll);
+    h1mc_bmass       -> Fill(Bmass);
+    h1mc_xgb         -> Fill(xgb);
+    h1mc_L1id        -> Fill(L1id);
+    h1mc_L2id        -> Fill(L2id);   
+    h1mc_Bprob       -> Fill(Bprob);  
+    h1mc_BsLxy       -> Fill(BsLxy);    
+    h1mc_Bcos        -> Fill(Bcos);     
+    h1mc_L1pt        -> Fill(L1pt);     
+    h1mc_L2pt        -> Fill(L2pt);  
+    h1mc_Kpt         -> Fill(Kpt);      
+    h1mc_LKdz        -> Fill(LKdz);  
+    h1mc_L1L2dr      -> Fill(L1L2dr);   
+    h1mc_LKdr        -> Fill(LKdr);  
+    h1mc_L1iso       -> Fill(L1iso);  
+    h1mc_Kiso        -> Fill(Kiso);  
+    h1mc_Passymetry  -> Fill(Passymetry);  
+    h1mc_Kip3d       -> Fill(Kip3d);  
+
+  } // Loop over entries
+
+
+  // Save outputs
+  TFile myfile("myFileMC.root","RECREATE");
+  myfile.cd();
+  h1mc_mll         -> Write();
+  h1mc_bmass       -> Write();
+  h1mc_xgb         -> Write();
+  h1mc_L1id        -> Write();  
+  h1mc_L2id        -> Write();  
+  h1mc_Bprob       -> Write();  
+  h1mc_BsLxy       -> Write();  
+  h1mc_Bcos        -> Write();  
+  h1mc_L1pt        -> Write();  
+  h1mc_L2pt        -> Write();  
+  h1mc_Kpt         -> Write();  
+  h1mc_LKdz        -> Write();  
+  h1mc_L1L2dr      -> Write();  
+  h1mc_LKdr        -> Write();  
+  h1mc_L1iso       -> Write();  
+  h1mc_Kiso        -> Write();  
+  h1mc_Passymetry  -> Write();  
+  h1mc_Kip3d       -> Write();  
+  myfile.Close();
+}
---
+++ splots/distribMC.h
@@ -0,0 +1,202 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Thu Feb  3 16:15:10 2022 by ROOT version 6.24/06
+// from TTree mytreefit/mytreefit
+// found on file: ottoPFPF/forMeas_xgbmodel_kee_12B_kee_correct_pu_Depth17_PFe_v7.3_0_MCres.root
+//////////////////////////////////////////////////////////
+
+#ifndef distribMC_h
+#define distribMC_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+class distribMC {
+ public :
+  TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+  Int_t           fCurrent; //!current Tree number in a TChain
+  
+  // Fixed size dimensions of array or collections stored in the TTree if any.
+  
+  // Declaration of leaf types
+  Double_t        xgb;
+  Float_t         Bmass;
+  Float_t         Mll;
+  Float_t         Npv;
+  Float_t         Bprob;
+  Float_t         BsLxy;
+  Float_t         L1pt;
+  Float_t         L2pt;
+  Float_t         Kpt;
+  Float_t         Bcos;
+  Float_t         LKdz;
+  Float_t         L1L2dr;
+  Float_t         LKdr;
+  Float_t         L2id;
+  Float_t         Kiso;
+  Float_t         BBDphi;
+  Float_t         BTrkdxy2;
+  Float_t         L1id;
+  Float_t         L1iso;
+  Float_t         KLmassD0;
+  Float_t         Passymetry;
+  Float_t         Kip3d;
+  Float_t         Kip3dErr;
+  Float_t         Mu8_IP3;
+  Float_t         Mu8_IP5;
+  Float_t         Mu8_IP6;
+  Float_t         Mu9_IP5;
+  Float_t         Mu9_IP6;
+  Float_t         Mu12_IP6;
+  
+  // List of branches
+  TBranch        *b_xgb;   //!
+  TBranch        *b_Bmass;   //!
+  TBranch        *b_Mll;   //!
+  TBranch        *b_Npv;   //!
+  TBranch        *b_Bprob;   //!
+  TBranch        *b_BsLxy;   //!
+  TBranch        *b_L1pt;   //!
+  TBranch        *b_L2pt;   //!
+  TBranch        *b_Kpt;   //!
+  TBranch        *b_Bcos;   //!
+  TBranch        *b_LKdz;   //!
+  TBranch        *b_L1L2dr;   //!
+  TBranch        *b_LKdr;   //!
+  TBranch        *b_L2id;   //!
+  TBranch        *b_Kiso;   //!
+  TBranch        *b_BBDphi;   //!
+  TBranch        *b_BTrkdxy2;   //!
+  TBranch        *b_L1id;   //!
+  TBranch        *b_L1iso;   //!
+  TBranch        *b_KLmassD0;   //!
+  TBranch        *b_Passymetry;   //!
+  TBranch        *b_Kip3d;   //!
+  TBranch        *b_Kip3dErr;   //!
+  TBranch        *b_Mu8_IP3;   //!
+  TBranch        *b_Mu8_IP5;   //!
+  TBranch        *b_Mu8_IP6;   //!
+  TBranch        *b_Mu9_IP5;   //!
+  TBranch        *b_Mu9_IP6;   //!
+  TBranch        *b_Mu12_IP6;   //!
+  
+  distribMC(TTree *tree=0);
+  virtual ~distribMC();
+  virtual Int_t    Cut(Long64_t entry);
+  virtual Int_t    GetEntry(Long64_t entry);
+  virtual Long64_t LoadTree(Long64_t entry);
+  virtual void     Init(TTree *tree);
+  virtual void     Loop(int q2bin, float bdtCut);
+  virtual Bool_t   Notify();
+  virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef distribMC_cxx
+distribMC::distribMC(TTree *tree) : fChain(0) 
+{
+  // if parameter tree is not specified (or zero), connect the file
+  // used to generate this class and read the Tree.
+  if (tree == 0) {          
+    TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("ottoPFPFnonReg/forMeas_xgbmodel_kee_12B_kee_correct_pu_Depth17_PFe_v7.3_nonreg_ottoCut_0_MCres.root");
+    if (!f || !f->IsOpen()) {
+      f = new TFile("ottoPFPFnonReg/forMeas_xgbmodel_kee_12B_kee_correct_pu_Depth17_PFe_v7.3_nonreg_ottoCut_0_MCres.root");
+    }
+    f->GetObject("mytreefit",tree);
+    
+  }
+  Init(tree);
+}
+
+distribMC::~distribMC()
+{
+  if (!fChain) return;
+  delete fChain->GetCurrentFile();
+}
+
+Int_t distribMC::GetEntry(Long64_t entry)
+{
+  // Read contents of entry.
+  if (!fChain) return 0;
+  return fChain->GetEntry(entry);
+}
+
+Long64_t distribMC::LoadTree(Long64_t entry)
+{
+  // Set the environment to read one entry
+  if (!fChain) return -5;
+  Long64_t centry = fChain->LoadTree(entry);
+  if (centry < 0) return centry;
+  if (fChain->GetTreeNumber() != fCurrent) {
+    fCurrent = fChain->GetTreeNumber();
+    Notify();
+  }
+  return centry;
+}
+
+void distribMC::Init(TTree *tree)
+{
+  // Set branch addresses and branch pointers
+  if (!tree) return;
+  fChain = tree;
+  fCurrent = -1;
+  fChain->SetMakeClass(1);
+  
+  fChain->SetBranchAddress("xgb", &xgb, &b_xgb);
+  fChain->SetBranchAddress("Bmass", &Bmass, &b_Bmass);
+  fChain->SetBranchAddress("Mll", &Mll, &b_Mll);
+  fChain->SetBranchAddress("Npv", &Npv, &b_Npv);
+  fChain->SetBranchAddress("Bprob", &Bprob, &b_Bprob);
+  fChain->SetBranchAddress("BsLxy", &BsLxy, &b_BsLxy);
+  fChain->SetBranchAddress("L1pt", &L1pt, &b_L1pt);
+  fChain->SetBranchAddress("L2pt", &L2pt, &b_L2pt);
+  fChain->SetBranchAddress("Kpt", &Kpt, &b_Kpt);
+  fChain->SetBranchAddress("Bcos", &Bcos, &b_Bcos);
+  fChain->SetBranchAddress("LKdz", &LKdz, &b_LKdz);
+  fChain->SetBranchAddress("L1L2dr", &L1L2dr, &b_L1L2dr);
+  fChain->SetBranchAddress("LKdr", &LKdr, &b_LKdr);
+  fChain->SetBranchAddress("L2id", &L2id, &b_L2id);
+  fChain->SetBranchAddress("Kiso", &Kiso, &b_Kiso);
+  fChain->SetBranchAddress("BBDphi", &BBDphi, &b_BBDphi);
+  fChain->SetBranchAddress("BTrkdxy2", &BTrkdxy2, &b_BTrkdxy2);
+  fChain->SetBranchAddress("L1id", &L1id, &b_L1id);
+  fChain->SetBranchAddress("L1iso", &L1iso, &b_L1iso);
+  fChain->SetBranchAddress("KLmassD0", &KLmassD0, &b_KLmassD0);
+  fChain->SetBranchAddress("Passymetry", &Passymetry, &b_Passymetry);
+  fChain->SetBranchAddress("Kip3d", &Kip3d, &b_Kip3d);
+  fChain->SetBranchAddress("Kip3dErr", &Kip3dErr, &b_Kip3dErr);
+  fChain->SetBranchAddress("Mu8_IP3", &Mu8_IP3, &b_Mu8_IP3);
+  fChain->SetBranchAddress("Mu8_IP5", &Mu8_IP5, &b_Mu8_IP5);
+  fChain->SetBranchAddress("Mu8_IP6", &Mu8_IP6, &b_Mu8_IP6);
+  fChain->SetBranchAddress("Mu9_IP5", &Mu9_IP5, &b_Mu9_IP5);
+  fChain->SetBranchAddress("Mu9_IP6", &Mu9_IP6, &b_Mu9_IP6);
+  fChain->SetBranchAddress("Mu12_IP6", &Mu12_IP6, &b_Mu12_IP6);
+  Notify();
+}
+
+Bool_t distribMC::Notify()
+{
+  return kTRUE;
+}
+
+void distribMC::Show(Long64_t entry)
+{
+  // Print contents of entry.
+  // If entry is not specified, print current entry
+  if (!fChain) return;
+  fChain->Show(entry);
+}
+
+Int_t distribMC::Cut(Long64_t entry)
+{
+  // This function may be called from Loop.
+  // returns  1 if entry is accepted.
+  // returns -1 otherwise.
+  return 1;
+}
+
+#endif // #ifdef distribMC_cxx
