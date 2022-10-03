4601fbaac44a875d490dedd94ef7493e1331e2a7
crovelli <chiara.rovelli@cern.ch>
2022-10-03 10:19:18 +0200

    first commit

---
+++ splots/weightAdditionToMCTree.cc
@@ -0,0 +1,363 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+#include <TH1.h>
+#include <iostream>
+#include <vector>
+#include <TRandom.h>
+#include "TLorentzVector.h"
+
+using namespace std;
+
+double GetEle1ptWeight(double ele1pt);
+double GetEle2ptWeight(double ele2pt);
+double GetKptWeight(double kpt);
+double GetElesDrWeight(double elesdr);
+
+// For weights application
+std::vector<Double_t> anweightEle1pt_;
+std::vector<Double_t> anweightEle2pt_;
+std::vector<Double_t> anweightKpt_;
+std::vector<Double_t> anweightElesDr_;
+std::vector<Double_t> lowedgeEle1pt_;
+std::vector<Double_t> lowedgeEle2pt_;
+std::vector<Double_t> lowedgeKpt_;
+std::vector<Double_t> lowedgeElesDr_;
+
+void weightAdditionToTree(const char* dataFilename, const char* ratioFilename) {
+
+  cout << "Formatting " << dataFilename << " adding weights" << endl;  
+
+  // Loading JPsi/LowQ2 ratios to compute weights
+  TFile *ratioFileMCBins = 0;
+  ratioFileMCBins = TFile::Open(TString("./")+TString(ratioFilename));
+  TH1F *ratioEle1pt_fromFile = (TH1F*)ratioFileMCBins->Get("ratio_Ele1Pt-JPsiToLowQ2");
+  TH1F *ratioEle2pt_fromFile = (TH1F*)ratioFileMCBins->Get("ratio_Ele2Pt-JPsiToLowQ2");
+  TH1F *ratioKpt_fromFile    = (TH1F*)ratioFileMCBins->Get("ratio_KPt-JPsiToLowQ2");
+  TH1F *ratioElesDr_fromFile = (TH1F*)ratioFileMCBins->Get("ratio_ElesDr-JPsiToLowQ2");
+
+
+  // Original ntuple   
+  TFile *fileOrig = 0;
+  TTree *treeOrig = 0;
+
+  fileOrig = TFile::Open(TString("./")+TString(dataFilename));
+  if( fileOrig ) {
+    fileOrig->cd();
+    treeOrig = (TTree*)fileOrig->Get("mytreefit");
+  } else {
+    cout << "File " << dataFilename << " not existing !" << endl;
+    return;
+  }
+  
+  fileOrig->cd();
+  if (!treeOrig) {
+    cout << "mytreefit not existing !" << endl; 
+    return;    
+  }
+
+  treeOrig->SetMakeClass(0);
+  cout << "TreeOrig->Size = "<< treeOrig->GetEntries() << endl;
+  
+  // number of entries saved in the first tree
+  int nentriesOrig = treeOrig->GetEntries();   
+  
+  // Tree for the final format
+  TFile *fileNew = TFile::Open(TString("./Formatted_Check_")+TString(dataFilename),"recreate");
+  fileNew->ls();
+  fileNew->cd();
+  TTree *treeNew = new TTree("mytreefit","mytreefit with weight");
+  treeNew->SetAutoSave(-99999999999);
+  treeNew->SetAutoFlush(-99999999999);
+
+  std::vector<TTree*> trees; 
+  trees.push_back(treeNew);
+
+  // original tree leaves
+  Double_t        xgb = 0.;
+  Float_t         Bmass = 0.;
+  Float_t         Mll = 0.;
+  Float_t         Npv = 0.;
+  Float_t         Bprob = 0.;
+  Float_t         BsLxy = 0.;
+  Float_t         L1pt = 0.;
+  Float_t         L2pt = 0.;
+  Float_t         Kpt = 0.;
+  Float_t         Bcos = 0.;
+  Float_t         LKdz = 0.;
+  Float_t         L1L2dr = 0.;
+  Float_t         LKdr = 0.;
+  Float_t         L2id = 0.;
+  Float_t         Kiso = 0.;
+  Float_t         BBDphi = 0.;
+  Float_t         BTrkdxy2 = 0.;
+  Float_t         L1id = 0.;
+  Float_t         L1iso = 0.;
+  Float_t         KLmassD0 = 0.;
+  Float_t         Passymetry = 0.;
+  Float_t         Kip3d = 0.;
+  Float_t         Kip3dErr = 0.;
+  Float_t         Bpt = 0.;
+  Float_t         Mu7_IP4 = 0.;
+  Float_t         Mu8_IP3 = 0.;
+  Float_t         Mu8_IP5 = 0.;
+  Float_t         Mu8_IP6 = 0.;
+  Float_t         Mu9_IP5 = 0.;
+  Float_t         Mu9_IP6 = 0.;
+  Float_t         Mu12_IP6 = 0.;
+
+  // List of branches - original tree
+  TBranch        *b_xgb;   //!
+  TBranch        *b_Bmass;   //!
+  TBranch        *b_Mll;   //!
+  TBranch        *b_Npv;   //!
+  TBranch        *b_Bprob;   //!
+  TBranch        *b_BsLxy;   //!
+  TBranch        *b_L1pt;   //!
+  TBranch        *b_L2pt;   //!
+  TBranch        *b_Kpt;   //!
+  TBranch        *b_Bcos;   //!
+  TBranch        *b_LKdz;   //!
+  TBranch        *b_L1L2dr;   //!
+  TBranch        *b_LKdr;   //!
+  TBranch        *b_L2id;   //!
+  TBranch        *b_Kiso;   //!
+  TBranch        *b_BBDphi;   //!
+  TBranch        *b_BTrkdxy2;   //!
+  TBranch        *b_L1id;   //!
+  TBranch        *b_L1iso;   //!
+  TBranch        *b_KLmassD0;   //!
+  TBranch        *b_Passymetry;   //!
+  TBranch        *b_Kip3d;   //!
+  TBranch        *b_Kip3dErr;   //!
+  TBranch        *b_Bpt;   //!
+  TBranch        *b_Mu7_IP4;   //!
+  TBranch        *b_Mu8_IP3;   //!
+  TBranch        *b_Mu8_IP5;   //!
+  TBranch        *b_Mu8_IP6;   //!
+  TBranch        *b_Mu9_IP5;   //!
+  TBranch        *b_Mu9_IP6;   //!
+  TBranch        *b_Mu12_IP6;   //!
+
+  // Set branch addresses and branch pointers 
+  treeOrig->SetBranchAddress("xgb", &xgb, &b_xgb);
+  treeOrig->SetBranchAddress("Bmass", &Bmass, &b_Bmass);
+  treeOrig->SetBranchAddress("Mll", &Mll, &b_Mll);
+  treeOrig->SetBranchAddress("Npv", &Npv, &b_Npv);
+  treeOrig->SetBranchAddress("Bprob", &Bprob, &b_Bprob);
+  treeOrig->SetBranchAddress("BsLxy", &BsLxy, &b_BsLxy);
+  treeOrig->SetBranchAddress("L1pt", &L1pt, &b_L1pt);
+  treeOrig->SetBranchAddress("L2pt", &L2pt, &b_L2pt);
+  treeOrig->SetBranchAddress("Kpt", &Kpt, &b_Kpt);
+  treeOrig->SetBranchAddress("Bcos", &Bcos, &b_Bcos);
+  treeOrig->SetBranchAddress("LKdz", &LKdz, &b_LKdz);
+  treeOrig->SetBranchAddress("L1L2dr", &L1L2dr, &b_L1L2dr);
+  treeOrig->SetBranchAddress("LKdr", &LKdr, &b_LKdr);
+  treeOrig->SetBranchAddress("L2id", &L2id, &b_L2id);
+  treeOrig->SetBranchAddress("Kiso", &Kiso, &b_Kiso);
+  treeOrig->SetBranchAddress("BBDphi", &BBDphi, &b_BBDphi);
+  treeOrig->SetBranchAddress("BTrkdxy2", &BTrkdxy2, &b_BTrkdxy2);
+  treeOrig->SetBranchAddress("L1id", &L1id, &b_L1id);
+  treeOrig->SetBranchAddress("L1iso", &L1iso, &b_L1iso);
+  treeOrig->SetBranchAddress("KLmassD0", &KLmassD0, &b_KLmassD0);
+  treeOrig->SetBranchAddress("Passymetry", &Passymetry, &b_Passymetry);
+  treeOrig->SetBranchAddress("Kip3d", &Kip3d, &b_Kip3d);
+  treeOrig->SetBranchAddress("Kip3dErr", &Kip3dErr, &b_Kip3dErr);
+  treeOrig->SetBranchAddress("Bpt", &Bpt, &b_Bpt);
+  treeOrig->SetBranchAddress("Mu7_IP4", &Mu7_IP4, &b_Mu7_IP4);
+  treeOrig->SetBranchAddress("Mu8_IP3", &Mu8_IP3, &b_Mu8_IP3);
+  treeOrig->SetBranchAddress("Mu8_IP5", &Mu8_IP5, &b_Mu8_IP5);
+  treeOrig->SetBranchAddress("Mu8_IP6", &Mu8_IP6, &b_Mu8_IP6);
+  treeOrig->SetBranchAddress("Mu9_IP5", &Mu9_IP5, &b_Mu9_IP5);
+  treeOrig->SetBranchAddress("Mu9_IP6", &Mu9_IP6, &b_Mu9_IP6);
+  treeOrig->SetBranchAddress("Mu12_IP6", &Mu12_IP6, &b_Mu12_IP6);
+
+  // New variables
+  Double_t weightElePt1;
+  Double_t weightElePt2;
+  Double_t weightKPt;
+  Double_t weightElesDr;
+
+  // New branches
+  for(int i=0; i<(int)trees.size();i++) {
+    TTree *theTreeNew = trees[i];
+    theTreeNew->Branch("weightElePt1", &weightElePt1, "weightElePt1/D");
+    theTreeNew->Branch("weightElePt2", &weightElePt2, "weightElePt2/D");
+    theTreeNew->Branch("weightKPt", &weightKPt, "weightKPt/D");
+    theTreeNew->Branch("weightElesDr", &weightElesDr, "weightElesDr/D");
+    theTreeNew->Branch("xgb", &xgb, "xgb/D");
+    theTreeNew->Branch("Bmass", &Bmass, "Bmass/F");
+    theTreeNew->Branch("Mll", &Mll, "Mll/F");
+    theTreeNew->Branch("Npv", &Npv, "Npv/F");
+    theTreeNew->Branch("Bprob", &Bprob, "Bprob/F");
+    theTreeNew->Branch("BsLxy", &BsLxy, "BsLxy/F");
+    theTreeNew->Branch("L1pt", &L1pt, "L1pt/F");
+    theTreeNew->Branch("L2pt", &L2pt, "L2pt/F");
+    theTreeNew->Branch("Kpt", &Kpt, "Kpt/F");
+    theTreeNew->Branch("Bcos", &Bcos, "Bcos/F");
+    theTreeNew->Branch("LKdz", &LKdz, "LKdz/F");
+    theTreeNew->Branch("L1L2dr", &L1L2dr, "L1L2dr/F");
+    theTreeNew->Branch("LKdr", &LKdr, "LKdr/F");
+    theTreeNew->Branch("L2id", &L2id, "L2id/F");
+    theTreeNew->Branch("Kiso", &Kiso, "Kiso/F");
+    theTreeNew->Branch("BBDphi", &BBDphi, "BBDphi/F");
+    theTreeNew->Branch("BTrkdxy2", &BTrkdxy2, "BTrkdxy2/F");
+    theTreeNew->Branch("L1id", &L1id, "L1id/F");
+    theTreeNew->Branch("L1iso", &L1iso, "L1iso/F");
+    theTreeNew->Branch("KLmassD0", &KLmassD0, "KLmassD0/F");
+    theTreeNew->Branch("Passymetry", &Passymetry, "Passymetry/F");
+    theTreeNew->Branch("Kip3d", &Kip3d, "Kip3d/F");
+    theTreeNew->Branch("Kip3dErr", &Kip3dErr, "Kip3dErr/F");
+    theTreeNew->Branch("Bpt", &Bpt, "Bpt/F");
+    theTreeNew->Branch("Mu7_IP4", &Mu7_IP4, "Mu7_IP4/F");
+    theTreeNew->Branch("Mu8_IP3", &Mu8_IP3, "Mu8_IP3/F");
+    theTreeNew->Branch("Mu8_IP5", &Mu8_IP5, "Mu8_IP5/F");
+    theTreeNew->Branch("Mu8_IP6", &Mu8_IP6, "Mu8_IP6/F");
+    theTreeNew->Branch("Mu9_IP5", &Mu9_IP5, "Mu9_IP5/F");
+    theTreeNew->Branch("Mu9_IP6", &Mu9_IP6, "Mu9_IP6/F");
+    theTreeNew->Branch("Mu12_IP6", &Mu12_IP6, "Mu12_IP6/F");
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Load weights: ele1 pt
+  for (int i = 0; i<ratioEle1pt_fromFile->GetNbinsX(); i++) {
+
+    double myweightEle1pt=ratioEle1pt_fromFile->GetBinContent(i+1);
+    anweightEle1pt_.push_back(myweightEle1pt);              
+
+    double mylowedgeEle1pt=ratioEle1pt_fromFile->GetBinLowEdge(i+1); 
+    lowedgeEle1pt_.push_back(mylowedgeEle1pt);
+  }
+
+  // Load weights: ele2 pt
+  for (int i = 0; i<ratioEle2pt_fromFile->GetNbinsX(); i++) {
+
+    double myweightEle2pt=ratioEle2pt_fromFile->GetBinContent(i+1);
+    anweightEle2pt_.push_back(myweightEle2pt);              
+
+    double mylowedgeEle2pt=ratioEle2pt_fromFile->GetBinLowEdge(i+1); 
+    lowedgeEle2pt_.push_back(mylowedgeEle2pt);
+  }
+
+  // Load weights: K pt
+  for (int i = 0; i<ratioKpt_fromFile->GetNbinsX(); i++) {
+
+    double myweightKpt=ratioKpt_fromFile->GetBinContent(i+1);
+    anweightKpt_.push_back(myweightKpt);              
+
+    double mylowedgeKpt=ratioKpt_fromFile->GetBinLowEdge(i+1); 
+    lowedgeKpt_.push_back(mylowedgeKpt);
+  }
+
+  // Load weights: dR(ele1, ele2(
+  for (int i = 0; i<ratioElesDr_fromFile->GetNbinsX(); i++) {
+
+    double myweightElesDr=ratioElesDr_fromFile->GetBinContent(i+1);
+    anweightElesDr_.push_back(myweightElesDr);              
+
+    double mylowedgeElesDr=ratioElesDr_fromFile->GetBinLowEdge(i+1); 
+    lowedgeElesDr_.push_back(mylowedgeElesDr);
+  }
+
+
+  // Loop over events
+  cout << "Now preparing the new tree" << endl;
+  for(int i=0; i<nentriesOrig; i++) {
+    
+    if (i%10000 == 0) std::cout << ">>> Event # " << i << " / " << nentriesOrig << " entries" << std::endl; 
+    treeOrig->GetEntry(i);
+
+    // compute and add the weight
+    double OneOverWeightEle1pt = GetEle1ptWeight(L1pt);
+    double OneOverWeightEle2pt = GetEle2ptWeight(L2pt);
+    double OneOverWeightKpt    = GetKptWeight(Kpt);
+    double OneOverWeightElesDr = GetElesDrWeight(L1L2dr);
+    double thisEventWeightEle1pt, thisEventWeightEle2pt, thisEventWeightKpt, thisEventWeightElesDr;
+    if (OneOverWeightEle1pt!=0) 
+      thisEventWeightEle1pt = 1./OneOverWeightEle1pt;
+    else
+      thisEventWeightEle1pt = 0;
+
+    if (OneOverWeightEle2pt!=0) 
+      thisEventWeightEle2pt = 1./OneOverWeightEle2pt;
+    else
+      thisEventWeightEle2pt = 0;
+
+    if (OneOverWeightKpt!=0) 
+      thisEventWeightKpt = 1./OneOverWeightKpt;
+    else
+      thisEventWeightKpt = 0;
+
+    if (OneOverWeightElesDr!=0) 
+      thisEventWeightElesDr = 1./OneOverWeightElesDr;
+    else
+      thisEventWeightElesDr = 0;
+
+    weightElePt1 = thisEventWeightEle1pt;
+    weightElePt2 = thisEventWeightEle2pt;
+    weightKPt    = thisEventWeightKpt;
+    weightElesDr = thisEventWeightElesDr;
+
+    treeNew->Fill();
+  }
+
+  // new format
+  treeNew->Write();
+  fileNew->Close();
+  fileNew->ls();
+  
+  fileOrig->cd();
+  fileOrig->Close();  
+}
+
+double GetEle1ptWeight(double ele1pt) {
+
+  int thesizem1 = lowedgeEle1pt_.size()-1;
+  
+  double weight=1;
+  for (int i = 0; i<thesizem1; i++) {   
+    if (lowedgeEle1pt_[i]<=ele1pt && lowedgeEle1pt_[i+1]>ele1pt) weight = anweightEle1pt_[i];  
+  }
+  
+  return weight;
+}
+
+double GetEle2ptWeight(double ele2pt) {
+
+  int thesizem1 = lowedgeEle2pt_.size()-1;
+  
+  double weight=1;
+  for (int i = 0; i<thesizem1; i++) {   
+    if (lowedgeEle2pt_[i]<=ele2pt && lowedgeEle2pt_[i+1]>ele2pt) weight = anweightEle2pt_[i];  
+  }
+  
+  return weight;
+}
+
+double GetKptWeight(double kpt) {
+
+  int thesizem1 = lowedgeKpt_.size()-1;
+  
+  double weight=1;
+  for (int i = 0; i<thesizem1; i++) {   
+    if (lowedgeKpt_[i]<=kpt && lowedgeKpt_[i+1]>kpt) weight = anweightKpt_[i];  
+  }
+  
+  return weight;
+}
+
+double GetElesDrWeight(double elesdr) {
+
+  int thesizem1 = lowedgeElesDr_.size()-1;
+  
+  double weight=1;
+  for (int i = 0; i<thesizem1; i++) {   
+    if (lowedgeElesDr_[i]<=elesdr && lowedgeElesDr_[i+1]>elesdr) weight = anweightElesDr_[i];  
+  }
+  
+  return weight;
+}
+
+
+
