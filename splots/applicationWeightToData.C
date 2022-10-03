9f7f4f939e58ecab71fa41011951ca217c550c50
crovelli <chiara.rovelli@cern.ch>
2022-10-03 10:11:31 +0200

    more histos

--- splots/applicationWeightToData.C
+++ splots/applicationWeightToData.C
@@ -5,25 +5,33 @@
 #include <TCanvas.h>
 #include <TLegend.h>   
 
-void applicationWeightToData::Loop(int theVariable)       // theVariable must be coherent with .h : 0=ele1pt, 1=ele2pt, 2=elesDr
+void applicationWeightToData::Loop(int theVariable)       // theVariable must be coherent with .h : 0=ele1pt, 1=ele2pt, 2=kpt, 3=elesDr
 {
   TH1F *jPsiEle1pt_fromLoop_now = new TH1F("jPsiEle1pt_fromLoop_now","jPsiEle1pt_fromLoop_now", 80,  0.0, 40.);
   TH1F *jPsiEle1pt_fromLoop_ww  = new TH1F("jPsiEle1pt_fromLoop_ww", "jPsiEle1pt_fromLoop_ww",  80,  0.0, 40.);
   TH1F *jPsiEle2pt_fromLoop_now = new TH1F("jPsiEle2pt_fromLoop_now","jPsiEle2pt_fromLoop_now", 40,  0.0, 20.);
   TH1F *jPsiEle2pt_fromLoop_ww  = new TH1F("jPsiEle2pt_fromLoop_ww", "jPsiEle2pt_fromLoop_ww",  40,  0.0, 20.);
+  TH1F *jPsiKpt_fromLoop_now    = new TH1F("jPsiKpt_fromLoop_now","jPsiKpt_fromLoop_now", 40,  0.0, 20.);
+  TH1F *jPsiKpt_fromLoop_ww     = new TH1F("jPsiKpt_fromLoop_ww", "jPsiKpt_fromLoop_ww",  40,  0.0, 20.);
   TH1F *jPsiElesDr_fromLoop_now = new TH1F("jPsiElesDr_fromLoop_now","jPsiElesDr_fromLoop_now", 20,  0.0, 2.);
   TH1F *jPsiElesDr_fromLoop_ww  = new TH1F("jPsiElesDr_fromLoop_ww", "jPsiElesDr_fromLoop_ww",  20,  0.0, 2.);
   TH1F *jPsiBmass_fromLoop_now  = new TH1F("jPsiBmass_fromLoop_now", "jPsiBmass_fromLoop_now",  50,  4.7, 5.7);
   TH1F *jPsiBmass_fromLoop_ww   = new TH1F("jPsiBmass_fromLoop_ww",  "jPsiBmass_fromLoop_ww",   50,  4.7, 5.7);
+  TH1F *jPsiXgb_fromLoop_now    = new TH1F("jPsiXgb_fromLoop_now", "jPsiXgb_fromLoop_now",  45,  0.0, 15.);
+  TH1F *jPsiXgb_fromLoop_ww     = new TH1F("jPsiXgb_fromLoop_ww",  "jPsiXgb_fromLoop_ww",   45,  0.0, 15.);
   //
   jPsiEle1pt_fromLoop_now -> Sumw2();
   jPsiEle1pt_fromLoop_ww  -> Sumw2();
   jPsiEle2pt_fromLoop_now -> Sumw2();
   jPsiEle2pt_fromLoop_ww  -> Sumw2();
+  jPsiKpt_fromLoop_now    -> Sumw2();
+  jPsiKpt_fromLoop_ww     -> Sumw2();
   jPsiElesDr_fromLoop_now -> Sumw2();
   jPsiElesDr_fromLoop_ww  -> Sumw2();
   jPsiBmass_fromLoop_now  -> Sumw2();
   jPsiBmass_fromLoop_ww   -> Sumw2();
+  jPsiXgb_fromLoop_now    -> Sumw2();
+  jPsiXgb_fromLoop_ww     -> Sumw2();
   //
   jPsiEle1pt_fromLoop_now -> Rebin(4);
   jPsiEle1pt_fromLoop_ww  -> Rebin(4);
@@ -36,26 +44,38 @@ void applicationWeightToData::Loop(int theVariable)       // theVariable must be
   jPsiEle1pt_fromLoop_ww  -> SetTitle("");
   jPsiEle2pt_fromLoop_now -> SetTitle("");
   jPsiEle2pt_fromLoop_ww  -> SetTitle("");
+  jPsiKpt_fromLoop_now    -> SetTitle("");
+  jPsiKpt_fromLoop_ww     -> SetTitle("");
   jPsiElesDr_fromLoop_now -> SetTitle("");
   jPsiElesDr_fromLoop_ww  -> SetTitle("");
   jPsiBmass_fromLoop_now  -> SetTitle("");
   jPsiBmass_fromLoop_ww   -> SetTitle("");
+  jPsiXgb_fromLoop_now    -> SetTitle("");
+  jPsiXgb_fromLoop_ww     -> SetTitle("");
   jPsiEle1pt_fromLoop_now -> SetLineColor(2);   
   jPsiEle1pt_fromLoop_ww  -> SetLineColor(1);   
   jPsiEle2pt_fromLoop_now -> SetLineColor(2);   
   jPsiEle2pt_fromLoop_ww  -> SetLineColor(1);   
+  jPsiKpt_fromLoop_now    -> SetLineColor(2);   
+  jPsiKpt_fromLoop_ww     -> SetLineColor(1);   
   jPsiElesDr_fromLoop_now -> SetLineColor(2);
   jPsiElesDr_fromLoop_ww  -> SetLineColor(1);
   jPsiBmass_fromLoop_now  -> SetLineColor(2);
   jPsiBmass_fromLoop_ww   -> SetLineColor(1);
+  jPsiXgb_fromLoop_now    -> SetLineColor(2);
+  jPsiXgb_fromLoop_ww     -> SetLineColor(1);
   jPsiEle1pt_fromLoop_now -> SetLineWidth(2);   
   jPsiEle1pt_fromLoop_ww  -> SetLineWidth(2);   
   jPsiEle2pt_fromLoop_now -> SetLineWidth(2);   
   jPsiEle2pt_fromLoop_ww  -> SetLineWidth(2);   
+  jPsiKpt_fromLoop_now    -> SetLineWidth(2);   
+  jPsiKpt_fromLoop_ww     -> SetLineWidth(2);   
   jPsiElesDr_fromLoop_now -> SetLineWidth(2);
   jPsiElesDr_fromLoop_ww  -> SetLineWidth(2);
   jPsiBmass_fromLoop_now  -> SetLineWidth(2);
   jPsiBmass_fromLoop_ww   -> SetLineWidth(2);
+  jPsiXgb_fromLoop_now    -> SetLineWidth(2);
+  jPsiXgb_fromLoop_ww     -> SetLineWidth(2);
   
 
   if (fChain == 0) return;
@@ -68,22 +88,30 @@ void applicationWeightToData::Loop(int theVariable)       // theVariable must be
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
 
-    // JPsi Q2 range   
+    // Selection (denominator)
     if (Mll>3.2 || Mll<2.9) continue;
-
-    // Same selection as applied in data 
     if (Bmass>5.7 || Bmass<4.7) continue;
     if (xgb<0)        continue;
-    // if (KLmassD0<2.0) continue;      
+    if (KLmassD0<2.0) continue;      
+
+    float weight;
+    if (theVariable==0) weight = weightElePt1;
+    if (theVariable==1) weight = weightElePt2;
+    if (theVariable==2) weight = weightKPt;
+    if (theVariable==3) weight = weightElesDr;
 
     jPsiEle1pt_fromLoop_now->Fill(L1pt);
     jPsiEle1pt_fromLoop_ww ->Fill(L1pt, weight);       
     jPsiEle2pt_fromLoop_now->Fill(L2pt);
     jPsiEle2pt_fromLoop_ww ->Fill(L2pt, weight);       
+    jPsiKpt_fromLoop_now   ->Fill(Kpt);
+    jPsiKpt_fromLoop_ww    ->Fill(Kpt, weight);       
     jPsiElesDr_fromLoop_now->Fill(L1L2dr);
     jPsiElesDr_fromLoop_ww ->Fill(L1L2dr, weight);       
     jPsiBmass_fromLoop_now ->Fill(Bmass);
     jPsiBmass_fromLoop_ww  ->Fill(Bmass, weight);       
+    jPsiXgb_fromLoop_now   ->Fill(xgb);
+    jPsiXgb_fromLoop_ww    ->Fill(xgb, weight);       
   }
 
 
@@ -115,17 +143,35 @@ void applicationWeightToData::Loop(int theVariable)       // theVariable must be
 
   if (theVariable==2) {
     TCanvas c2c("c2c","With and wo weights",1);
+    jPsiKpt_fromLoop_ww  -> DrawNormalized("hist");
+    jPsiKpt_fromLoop_now -> DrawNormalized("samehist");
+    leg2.Draw();
+    c2c.SaveAs("Kpt_withAndWoWeight.png");
+  }
+
+  if (theVariable==3) {
+    TCanvas c2d("c2d","With and wo weights",1);
     jPsiElesDr_fromLoop_ww  -> DrawNormalized("hist");
     jPsiElesDr_fromLoop_now -> DrawNormalized("samehist");
     leg2.Draw();
-    c2c.SaveAs("ElesDr_withAndWoWeight.png");
+    c2d.SaveAs("ElesDr_withAndWoWeight.png");
   }
 
-  TCanvas c2d("c2d","With and wo weights",1);
+  TCanvas c2e("c2e","With and wo weights",1);
   jPsiBmass_fromLoop_ww  -> DrawNormalized("hist");
   jPsiBmass_fromLoop_now -> DrawNormalized("samehist");
   leg2.Draw();
-  if (theVariable==0) c2d.SaveAs("Bmass_withAndWoEle1PtWeight.png");
-  if (theVariable==1) c2d.SaveAs("Bmass_withAndWoEle2PtWeight.png");
-  if (theVariable==2) c2d.SaveAs("Bmass_withAndWoElesDrWeight.png");
+  if (theVariable==0) c2e.SaveAs("Bmass_withAndWoEle1PtWeight.png");
+  if (theVariable==1) c2e.SaveAs("Bmass_withAndWoEle2PtWeight.png");
+  if (theVariable==2) c2e.SaveAs("Bmass_withAndWoKPtWeight.png");
+  if (theVariable==3) c2e.SaveAs("Bmass_withAndWoElesDrWeight.png");
+
+  TCanvas c2f("c2f","With and wo weights",1);
+  jPsiXgb_fromLoop_ww  -> DrawNormalized("hist");
+  jPsiXgb_fromLoop_now -> DrawNormalized("samehist");
+  leg2.Draw();
+  if (theVariable==0) c2f.SaveAs("Xgb_withAndWoEle1PtWeight.png");
+  if (theVariable==1) c2f.SaveAs("Xgb_withAndWoEle2PtWeight.png");
+  if (theVariable==2) c2f.SaveAs("Xgb_withAndWoKPtWeight.png");
+  if (theVariable==3) c2f.SaveAs("Xgb_withAndWoElesDrWeight.png");
 }
